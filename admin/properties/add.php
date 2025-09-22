f761ced5fe0d603bce11e56d5c33c86710f7f2b7
Shaheer-20 <shaheervitla@gmail.com>
2025-09-22 10:58:58 +0530

    HEading changes

--- admin/properties/add.php
+++ admin/properties/add.php
@@ -389,7 +389,7 @@ function get_data($field) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Order</title>
+    <title>Add Property</title>
 <style>
         /* General Styling */
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
