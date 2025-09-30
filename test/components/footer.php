30116b6126d5acfd9649d2114cc733a70cf5521e
Shaheer-20 <shaheervitla@gmail.com>
2025-09-30 11:32:33 +0530

    fixed

--- test/components/footer.php
+++ test/components/footer.php
@@ -324,7 +324,9 @@
   <div class="row ">
 
     <div class="col-md-4 footer">
-    <img src="<?php echo $asset_path; ?>images/logo.png" alt="footer logo">
+    <a href="<?php echo $site_base_path; ?>" style="text-decoration: none;">
+      <img src="<?php echo $asset_path; ?>images/logo.png" alt="footer logo" >
+    </a>
   <p>Big Deal Real Estate is your trusted partner in buying, selling, and investing in properties. 
     We focus on transparency, professionalism, and making your real estate journey simple and hassle-free.</p>
 
